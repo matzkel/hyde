44ac1b9cd31095ec5b70d655e070bcb4ae2e2aee
Matvey <matzkel@proton.me>
2024-03-09 00:36:49 +0200

    Add BranchNode class and fix error handling bugs

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -9,7 +9,7 @@ class HTMLNode():
         raise NotImplementedError()
 
     def props_to_html(self):
-        if self.props is None:
+        if self.props is None or not self.props:
             raise ValueError("props dictionary is empty")
         
         props = []
@@ -21,15 +21,36 @@ class HTMLNode():
         return f"HTMLNode(\n{self.tag},\n{self.value},\n{self.children},\n{self.props}\n)"
 
 
+class BranchNode(HTMLNode):
+    def __init__(self, tag=None, childen=None):
+        super().__init__(tag, None, childen, None)
+
+    def to_html(self):
+        if self.tag is None or not self.tag:
+            raise ValueError("BranchNode requires tag")
+        
+        if self.children is None or not self.children:
+            raise ValueError("BranchNode requires children")
+
+        html_str = f"<{self.tag}>"
+        for child in self.children:
+            html_str += child.to_html()
+        html_str += f"</{self.tag}>"
+        return html_str
+    
+    def __repr__(self):
+        return f"BranchNode(\n{self.tag},\n{self.children}\n)"
+
+
 class LeafNode(HTMLNode):
     def __init__(self, tag=None, value=None, props=None):
         super().__init__(tag, value, None, props)
     
     def to_html(self):
         if self.value is None:
-            raise ValueError("Leaf node requires value")
+            raise ValueError("LeafNode requires value")
 
-        if self.tag is None: # If there is no tag, the value will be rendered as raw text
+        if self.tag is None or not self.tag: # If there is no tag, the value will be rendered as raw text
             return self.value
 
         try:
@@ -39,4 +60,4 @@ class LeafNode(HTMLNode):
             return f"<{self.tag}>{self.value}</{self.tag}>"
             
     def __repr__(self):
-        return f"LeafNode(\n{self.tag},\n{self.value},\n{self.props}\n)"
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
