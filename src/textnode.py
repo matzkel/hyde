1ed7c357c0391b37ae106a893723fb496fd560c9
Matvey <matzkel@proton.me>
2024-03-14 23:21:39 +0200

    Add function to convert from markdown into blocks

--- src/textnode.py
+++ src/textnode.py
@@ -57,6 +57,14 @@ def text_node_to_html_node(text_node):
         raise ValueError("TextNode should take TextType argument")
 
 
+def markdown_to_blocks(markdown):
+    blocks = []
+    for line in markdown.split("\n"):
+        result_line = line.strip() if line != '\n' else ""
+        blocks.append(result_line)
+    return blocks
+
+
 # SUPPORTS ONLY SINGLE LEVEL OF NESTING!!!
 # TODO: Add support for multiple levels of nesting
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
