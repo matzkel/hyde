db9e2c33b3734115c33fdae51c9e6a0227f69a50
Matvey <matzkel@proton.me>
2024-03-17 20:05:15 +0200

    Move stuff that are related to blocks into different file

---
+++ src/blocks.py
@@ -0,0 +1,30 @@
+class BlockType(Enum):
+    PARAGRAPH = 1
+    HEADING = 2
+    CODE = 3
+    QUOTE = 4
+    UNORDERED_LIST = 5
+    ORDERED_LIST = 6
+
+
+def block_to_block_type(block):
+    if block.startswith("#"):
+        return BlockType.HEADING
+    elif block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    elif block.startswith(">"):
+        return BlockType.QUOTE
+    elif block.startswith("*") or block.startswith("-"):
+        return BlockType.UNORDERED_LIST
+    elif block[:1].isdigit() and block[1:2].startswith("."):
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
+
+
+def markdown_to_blocks(markdown):
+    blocks = []
+    for line in markdown.split("\n"):
+        result_line = line.strip()
+        if result_line != "":
+            blocks.append(result_line)
+    return blocks
--- src/textnode.py
+++ src/textnode.py
@@ -4,15 +4,6 @@ from enum import Enum
 from htmlnode import LeafNode
 
 
-class BlockType(Enum):
-    PARAGRAPH = 1
-    HEADING = 2
-    CODE = 3
-    QUOTE = 4
-    UNORDERED_LIST = 5
-    ORDERED_LIST = 6
-
-
 class TextType(Enum):
     TEXT = 1
     BOLD = 2
@@ -64,29 +55,6 @@ def text_node_to_html_node(text_node):
         return LeafNode("img", None, {"src": text_node.url, "alt": text_node.text})
     else:
         raise ValueError("TextNode should take TextType argument")
-
-
-def block_to_block_type(block):
-    if block.startswith("#"):
-        return BlockType.HEADING
-    elif block.startswith("```") and block.endswith("```"):
-        return BlockType.CODE
-    elif block.startswith(">"):
-        return BlockType.QUOTE
-    elif block.startswith("*") or block.startswith("-"):
-        return BlockType.UNORDERED_LIST
-    elif block[:1].isdigit() and block[1:2].startswith("."):
-        return BlockType.ORDERED_LIST
-    return BlockType.PARAGRAPH
-
-
-def markdown_to_blocks(markdown):
-    blocks = []
-    for line in markdown.split("\n"):
-        result_line = line.strip()
-        if result_line != "":
-            blocks.append(result_line)
-    return blocks
 
 
 # SUPPORTS ONLY SINGLE LEVEL OF NESTING!!!
