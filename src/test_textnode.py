5f687d9d50b2ff7aee3cc5d83447c2043b5155b2
Matvey <matzkel@proton.me>
2024-03-17 20:08:47 +0200

    Move unit tests that are related to blocks into different file

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,14 +1,11 @@
 import unittest
 
 from textnode import (
-    BlockType,
     TextNode,
     TextType,
 
     text_to_text_node,
     text_node_to_html_node,
-    block_to_block_type,
-    markdown_to_blocks,
     split_nodes_delimiter,
     extract_markdown_images,
     extract_markdown_links,
@@ -154,57 +151,6 @@ class TestTextNode(unittest.TestCase):
                 TextNode(".", TextType.TEXT, None)
             ], text_to_text_node(text)
         )
-
-    def test_markdown_to_blocks(self):
-        markdown = """This is **bolded** paragraph
-
-        This is another paragraph with *italic* text and `code` here
-        This is the same paragraph on a new line
-
-        * This is a list
-        * with items"""
-        self.assertEqual(
-            [
-                "This is **bolded** paragraph",
-                "This is another paragraph with *italic* text and `code` here",
-                "This is the same paragraph on a new line",
-                "* This is a list",
-                "* with items"
-            ], markdown_to_blocks(markdown)
-        )
-
-    def test_markdown_to_blocks2(self):
-        markdown = """This is normal paragraph
-        This is another paragraph, but it includes **bold** and *italic* text
-        
-        * This is a list
-        * with item 1
-        * and item 2"""
-        self.assertEqual(
-            [
-                "This is normal paragraph",
-                "This is another paragraph, but it includes **bold** and *italic* text",
-                "* This is a list",
-                "* with item 1",
-                "* and item 2"
-            ], markdown_to_blocks(markdown)
-        )
-    
-    def test_block_to_block_type(self):
-        block = "1. This is an ordered list"
-        self.assertEqual(BlockType.ORDERED_LIST, block_to_block_type(block))
-
-    def test_block_to_block_type2(self):
-        block = "```This is not a code block!"
-        self.assertEqual(BlockType.PARAGRAPH, block_to_block_type(block))
-
-    def test_block_to_block_type3(self):
-        block = "###This is huge heading!"
-        self.assertEqual(BlockType.HEADING, block_to_block_type(block))
-
-    def test_block_to_block_type4(self):
-        block = ""
-        self.assertEqual(BlockType.PARAGRAPH, block_to_block_type(block))
 
 
 if __name__ == "__main__":
